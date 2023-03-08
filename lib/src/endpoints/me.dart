ab72dfab2de0c83d721fe3afc0b1b8a682a2c085
AlgoPioneer <richlemon29@gmail.com>
2023-03-08 12:27:42 +0100

    Update me.dart

--- lib/src/endpoints/me.dart
+++ lib/src/endpoints/me.dart
@@ -99,6 +99,34 @@ class Me extends EndpointPaging {
         (json) => PlayHistory.fromJson(json));
   }
 
+
+  /// Toggle Shuffle For User's Playback.
+  ///
+  /// Use [state] to toggle the shuffle. [true] to turn shuffle on and [false]
+  /// to turn it off respectively.
+  /// Returns the current player state by making another request.
+  /// See [player([String market])];
+  Future<Player> shuffle(bool state, [String? deviceId]) async {
+    return _api
+        ._put('v1/me/player/shuffle?' +
+            _buildQuery({'state': state, 'deviceId': deviceId}))
+        .then((response) {
+      if (response.isNotEmpty) {
+        // the success response of shuffle is always empty, therefore
+        // a non-empty response has to be an error
+        return Future.error(SpotifyError.fromJson(json.decode(response)));
+      }
+      return player();
+    });
+  }
+
+  Future<Player> player([String? market]) async {
+    var jsonString =
+        await _api._get('v1/me/player?' + _buildQuery({'market': market}));
+    final map = json.decode(jsonString);
+    return Player.fromJson(map);
+  }
+
   /// Get the current user's top tracks.
   Future<Iterable<Track>> topTracks() async {
     final jsonString = await _api._get('$_path/top/tracks');
@@ -146,7 +174,7 @@ class Me extends EndpointPaging {
     await _api._delete('$_path/shows?' + query);
   }
 
-  /// Check if passed albums (ids) are saved by current user. The output
+  /// Check if passed albums (ids) are saved by current user.
   /// [ids] the ist of id's to check
   /// Returns the list of id's mapped with the response whether it has been saved
   Future<Map<String, bool>> isSavedShows(List<String> ids) async {
